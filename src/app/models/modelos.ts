6b20b5781a5e764e318cd3739fb3b58a9cac445a
Thiara Palma <thiarapalma233@gmail.com>
2023-08-03 11:47:10 -0300

    primer commit

---
+++ src/app/models/modelos.ts
@@ -0,0 +1,12 @@
+export interface TarjetasInicio {
+    titulo: string;
+    descripcion: string;
+    imagen:string;
+    alt:string;
+}
+export interface tarjetasNosotros{
+    titulo:string;
+    descripcion:string;
+    imagen: string;
+    alt:string;
+}
